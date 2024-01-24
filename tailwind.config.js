12b49f4b9cad8db2d8abbd1ef6279ad618e56c27
Shubh <shubhjhawar78@gmail.com>
2024-01-24 14:45:31 -0500

    implemented first draft of homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        'sm': '640px', // Small screens
+        'md': '768px', // Medium screens
+        'lg': '1024px', // Large screens
+        'xl': '1280px', // Extra-large screens
       },
     },
   },
